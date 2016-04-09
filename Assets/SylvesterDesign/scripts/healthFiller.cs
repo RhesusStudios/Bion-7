63adb3d5f373e1f9e8978c9a3af5ade2dd1d0511
RhesusStudios <sylvesterhansen@h51.aau-1-day-2a.wlan.aalborguniversitet.dk>
2016-04-10 01:21:13 +0200

    YAAS
    I made the thing

---
+++ Assets/SylvesterDesign/scripts/healthFiller.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class healthFiller : MonoBehaviour {
+
+	public Image Air;
+	float airFill = 1;
+
+	float timer;
+	float delay;
+
+	// Use this for initialization
+	void Start () {
+		timer = Time.time;
+		delay = Random.Range (0.5f, 2f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if ((Time.time - timer) > delay) {
+			timer = Time.time;
+			delay = Random.Range (0.5f, 2f);
+			airFill -= 0.03f;
+			Air.fillAmount = airFill;
+		}
+	}
+
+	public void Boost(){
+		airFill -= 0.1f;
+		Air.fillAmount = airFill;
+	}
+
+	public void addAir(){
+		airFill += 0.15f;
+		Air.fillAmount = airFill;
+	}
+}
---
+++ Assets/SylvesterDesign/scripts/lookAt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lookAt : MonoBehaviour {
+
+
+	GameObject camCam;
+
+	// Use this for initialization
+	void Start () {
+
+		camCam = GameObject.Find ("Main Camera");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.LookAt (camCam.transform);
+	
+	}
+}
